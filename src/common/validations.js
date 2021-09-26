d1e5c3d6b8b0912b6d96e9b1605462163b27b588
Paulo H Rodrigues <qualvalordex@gmail.com>
2021-09-26 09:56:33 -0300

    :sparkles: add validation for jobs command

---
+++ src/common/validations.js
@@ -0,0 +1,15 @@
+import InvalidProvidedURL from "./errors/InvalidProvidedURL.js"
+
+function isValidUrl(string) {
+    let url
+
+    try {
+        url = new URL(string);
+    } catch (_) {
+        throw new InvalidProvidedURL(string)
+    }
+
+    return url.protocol === 'http:' || url.protocol === 'https:'
+}
+
+export { isValidUrl }
